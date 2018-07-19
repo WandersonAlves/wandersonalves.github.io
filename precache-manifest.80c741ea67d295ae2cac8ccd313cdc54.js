c3e37d1c9c46197dceee1da7979de8541d3e098c
WandersonAlves <wandersonalvesferreira2012@gmail.com>
2018-07-19 14:25:28 -0300

    new site

---
+++ precache-manifest.80c741ea67d295ae2cac8ccd313cdc54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "800e89f23d53b1365515",
+    "url": "./js/vendors~app.800e89f2.js"
+  },
+  {
+    "revision": "74689cc7773015e5e63a",
+    "url": "./js/app.74689cc7.js"
+  },
+  {
+    "revision": "31dc4480e507167c0031752fda201657",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6e63c1e156ec0df336f18fe176f57eec",
+    "url": "./img/avatar.6e63c1e1.jpg"
+  },
+  {
+    "revision": "74689cc7773015e5e63a",
+    "url": "./css/app.a69e2450.css"
+  }
+];
